9b59764cb4e5ef07ea4617ccdceb357307d7dcf7
Raphael <raphael.girao@unimedfortaleza.com.br>
2023-07-21 18:15:34 -0300

    Aula 01

--- src/services/authService.js
+++ src/services/authService.js
@@ -40,10 +40,28 @@ class AuthService {
   }
 
   async cadastrarUsuario(data) {
-    data.senha = await bcryptjs.hash(data.senha, 8);
-    
-    const usuario = new Usuario(data);
     try {
+      if (!data.nome) {
+        throw new Error('O nome de usuário é obrigatório!');
+      }
+  
+      if (!data.email) {
+        throw new Error('O email de usuário é obrigatório!');
+      }
+
+      if (!data.senha) {
+        throw new Error('A senha de usuário é obrigatório!');
+      }
+
+      const usuarioCadastrado = await Usuario.pegarPeloEmail(data.email);
+
+      if (usuarioCadastrado) {
+        throw new Error('O email já esta cadastrado!');
+      }
+
+      data.senha = await bcryptjs.hash(data.senha, 8);
+      
+      const usuario = new Usuario(data);
       const resposta = await usuario.salvar(usuario);
       return { message: 'usuario criado', content: resposta };
     } catch (err) {
